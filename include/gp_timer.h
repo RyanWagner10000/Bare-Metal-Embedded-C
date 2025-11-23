f78f39a82ebae989322d1b8da225b50027e5d04b
Ryan Wagner <ryan.wagner10000@gmail.com>
2025-11-23 12:33:18 -0700

    Added General Purpose Timer 2 & 5 with driver with half second delay

---
+++ include/gp_timer.h
@@ -0,0 +1,51 @@
+/*
+ * file: gp_timer.h
+ * description: header file for GP_TIMER peripheral structure
+ * author: Ryan Wagner
+ * date: November 22, 2025
+ * notes:
+ */
+
+#ifndef GP_TIMER_H
+#define GP_TIMER_H
+
+#include <stdint.h>
+
+#define TIM2_BASE 0x40000000
+
+#define TIM2 ((GP_TIMER_TypeDef *)TIM2_BASE)
+
+#define TIM2_EN (1U << 0)
+#define TIM2_CR1_EN (1U << 0)
+#define TIM2_SR_UIF (1U << 0)
+#define TIM2_PSC 16000
+#define TIM5_PSC 0
+#define TIM2_ARR 500
+#define TIM5_ARR 0
+
+typedef struct
+{
+    volatile uint32_t CR1;       // offset: 0x00
+    volatile uint32_t CR2;       // offset: 0x04
+    volatile uint32_t SMCR;      // offset: 0x08
+    volatile uint32_t DIER;      // offset: 0x0C
+    volatile uint32_t SR;        // offset: 0x10
+    volatile uint32_t EGR;       // offset: 0x14
+    volatile uint32_t CCMR1;     // offset: 0x18
+    volatile uint32_t CCMR2;     // offset: 0x1C
+    volatile uint32_t CCER;      // offset: 0x20
+    volatile uint32_t CNT;       // offset: 0x24
+    volatile uint32_t PSC;       // offset: 0x28
+    volatile uint32_t ARR;       // offset: 0x2C
+    volatile uint32_t RESERVED1; // offset: 0x30
+    volatile uint32_t CCR1;      // offset: 0x34
+    volatile uint32_t CCR2;      // offset: 0x38
+    volatile uint32_t CCR3;      // offset: 0x3C
+    volatile uint32_t CCR4;      // offset: 0x40
+    volatile uint32_t RESERVED2; // offset: 0x44
+    volatile uint32_t DCR;       // offset: 0x48
+    volatile uint32_t DMAR;      // offset: 0x4C
+    volatile uint32_t OR;        // offset: 0x50
+} GP_TIMER_TypeDef;
+
+#endif // GP_TIMER_H
--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 /*
  * file: main.c
- * description: fail that contains the main loop for the whole device
+ * description: file that contains the main loop for the whole device
  * author: Ryan Wagner
  * date: November 10, 2025
  * notes:
  */
 
 #include "main.h"
+#include "gp_timer.h"
 
 uint32_t DELAY_COUNT = 1000000 / 10;
 
@@ -41,6 +42,14 @@ void offLED(void)
     return;
 }
 
+void toggleLED(void)
+{
+    // Set output of PD13 High/Low
+    GPIOD->ODR ^= ORANGE_LED;
+
+    return;
+}
+
 void initPushButton(void)
 {
     // Enable cloack access to GPIOC
@@ -66,27 +75,58 @@ uint32_t getButtonState(void)
     return 0;
 }
 
+void initTIM2(void)
+{
+    // Enable clock access to General Purpose Timer 2
+    RCC->APB1ENR |= TIM2_EN;
+
+    // Set Prescaler
+    TIM2->PSC = TIM2_PSC - 1;
+    // Set Auto-reload Register
+    TIM2->ARR = TIM2_ARR - 1;
+    // Set counter to 0
+    TIM2->CNT = 0;
+
+    // Enable TIM2 on Control Register AFTER setting everything up
+    TIM2->CR1 = TIM2_CR1_EN;
+
+    return;
+}
+
 int main(void)
 {
     initLED();
-    initPushButton();
+    // initPushButton();
+    initTIM2();
 
-    uint32_t button_state = getButtonState();
+    // uint32_t button_state = getButtonState();
+
+    // while (1)
+    // {
+    //     button_state = getButtonState();
+        
+    //     if (button_state)
+    //     {
+    //         onLED();
+    //     }
+    //     else
+    //     {
+    //         offLED();
+    //     }
+    //     // for (uint32_t volatile i = 0; i < DELAY_COUNT; i++)
+    //     // {
+    //     // }
+    // }
 
     while (1)
     {
-        button_state = getButtonState();
-        
-        if (button_state)
-        {
-            onLED();
-        }
-        else
-        {
-            offLED();
-        }
-        // for (uint32_t volatile i = 0; i < DELAY_COUNT; i++)
-        // {
-        // }
+        toggleLED();
+
+        // Wait for timer to hit flag
+        while (!(TIM2->SR & TIM2_SR_UIF)){}
+
+        // Reset and clear
+        TIM2->SR &= ~TIM2_SR_UIF;
     }
+    
 }
