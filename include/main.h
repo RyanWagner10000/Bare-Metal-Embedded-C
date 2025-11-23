d97b3d8c71d38778243eb2533523715fa4ab7d5d
Ryan Wagner <ryan.wagner10000@gmail.com>
2025-11-22 23:11:53 -0700

    added push button logic to light

--- include/main.h
+++ include/main.h
@@ -14,16 +14,25 @@
 #include "../include/rcc.h"
 
 #define RCC_BASE 0x40023800
+#define GPIOA_BASE 0x40020000
 #define GPIOD_BASE 0x40020C00
 
 #define RCC ((RCC_TypeDef *)RCC_BASE)
+#define GPIOA ((GPIO_TypeDef *)GPIOA_BASE)
 #define GPIOD ((GPIO_TypeDef *)GPIOD_BASE)
 
-//  14: Bit mask for enabling GPIOD (bit 3)
+//  Bit mask for enabling GPIOD (bit 3)
 #define GPIODEN (1U << 3)
-//  15: Bit mask for GPIOD pin 13
+//  Bit mask for GPIOD pin 13
 #define PIN13 (1U << 13)
-//  16: Alias for PIN5 representing LED pin
-#define LED_PIN PIN13
+//  Alias for PIN13 representing LED pin
+#define ORANGE_LED PIN13
+
+//  Bit mask for enabling GPIOA (bit 0)
+#define GPIOAEN (1U << 0)
+//  Bit mask for GPIOA pin 0
+#define PIN0 (1U << 0)
+//  Alias for PIN0 representing Blue push button
+#define PUSH_BUTTON PIN0
 
 #endif // MAIN_H
--- src/main.c
+++ src/main.c
@@ -8,24 +8,85 @@
 
 #include "main.h"
 
-uint32_t DELAY_COUNT = 1000000/1;
+uint32_t DELAY_COUNT = 1000000 / 10;
 
-int main(void)
+uint32_t LED_BS13 = (1U << 13);
+uint32_t LED_BR13 = (1U << 29);
+
+void initLED(void)
 {
     // Enable clock access to GPIOD
     RCC->AHB1ENR |= GPIODEN;
 
-    // Set bit 26 to 1 for GPIO_D Mode Register
+    // Set PD13 mode to output
     GPIOD->MODER |= (1U << 26);
     GPIOD->MODER &= ~(1U << 27);
 
+    return;
+}
+
+void onLED(void)
+{
+    // Set PD13 High
+    GPIOD->BSRR |= LED_BS13;
+
+    return;
+}
+
+void offLED(void)
+{
+    // Set PD13 High
+    GPIOD->BSRR |= LED_BR13;
+
+    return;
+}
+
+void initPushButton(void)
+{
+    // Enable cloack access to GPIOC
+    RCC->AHB1ENR |= GPIOAEN;
+
+    // Set PD13 mode to output
+    GPIOA->MODER &= ~(1U << 0);
+    GPIOA->MODER &= ~(1U << 1);
+
+    return;
+}
+
+uint32_t getButtonState(void)
+{
+    // Button is active low
+
+    // Check if button is pushed
+    if (GPIOA->IDR & PUSH_BUTTON)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    initLED();
+    initPushButton();
+
+    uint32_t button_state = getButtonState();
+
     while (1)
     {
-        // Alternate PD13 (Orange LED Pin) high/low
-        GPIOD->ODR ^= LED_PIN;
-        for (uint32_t volatile i = 0; i < DELAY_COUNT; i++)
+        button_state = getButtonState();
+        
+        if (button_state)
         {
+            onLED();
         }
-
+        else
+        {
+            offLED();
+        }
+        // for (uint32_t volatile i = 0; i < DELAY_COUNT; i++)
+        // {
+        // }
     }
 }
